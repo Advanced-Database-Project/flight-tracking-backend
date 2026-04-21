3f246f5ef43b763c485ef1a600cfe204b9a44eae
DarshanKareliya <kareliyadarshan121@gmail.com>
2026-04-21 15:15:57 +0200

    changed project structure

--- apps/ride_share_api-gateway/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RideShareApiGatewayModule } from './ride_share_api-gateway.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(RideShareApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
