31e7db602645cdf9bdc5a8e2d1f6e34b9bb2c562
DarshanKareliya <kareliyadarshan121@gmail.com>
2026-04-29 11:54:34 +0200

    implemented web socket connection for flight tracking service

---
+++ services/flight-tracking-service/src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import http from "http";
+import { Server } from "socket.io";
+import env from "../../../shared/env.js";
+import connectMongoDB from "../../../shared/db.js";
+import flightTrackingRouter from "./routes/flightTrackingRoutes.js";
+
+
+
+const app = express();
+app.use(cors());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    methods: ["GET"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`A user connected from React. Socket ID: ${socket.id}`);
+});
+
+
+
+httpServer.listen(env.FLIGHT_TRACKING_SERIVCE_PORT || 5000, () => {
+  console.log(
+    `Flight Tracking Service started at ${env.FLIGHT_TRACKING_SERIVCE_PORT}`,
+  );
+});
