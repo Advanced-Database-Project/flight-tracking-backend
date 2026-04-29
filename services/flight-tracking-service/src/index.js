a5a85af8db8c62ed3d970e60d7d03d761cc1581f
DarshanKareliya <kareliyadarshan121@gmail.com>
2026-04-29 12:08:37 +0200

    added package.json and changed index.js

--- services/flight-tracking-service/src/index.js
+++ services/flight-tracking-service/src/index.js
@@ -1,13 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { createServer } from "http";
-import http from "http";
 import { Server } from "socket.io";
 import env from "../../../shared/env.js";
-import connectMongoDB from "../../../shared/db.js";
-import flightTrackingRouter from "./routes/flightTrackingRoutes.js";
-
-
 
 const app = express();
 app.use(cors());
@@ -21,8 +16,6 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   console.log(`A user connected from React. Socket ID: ${socket.id}`);
 });
-
-
 
 httpServer.listen(env.FLIGHT_TRACKING_SERIVCE_PORT || 5000, () => {
   console.log(
