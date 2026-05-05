ec1a421f0b98db7afbdef60de2ffd72473c1ffbb
deep <dipvadukiya06@gmail.com>
2026-05-05 15:34:18 +0200

    FTUC01: dataset migration: added script to adding the databse into the mongodb from the opensky datasets

--- db_migration.js
+++ db_migration.js
@@ -1,5 +1,7 @@
 //
 
+import fs from "fs";
+import csv from "csv-parser";
 import { MongoClient } from "mongodb";
 import env from "./shared/env.js";
 
@@ -11,9 +13,45 @@ async function run() {
   const db = client.db(env.MONGODB_DATABASE_NAME);
   const collection = db.collection("live_flight_tracking");
 
-  const result = await collection.find().toArray();
+  //   const result = await collection.find().toArray();
 
-  console.log(result);
+  //   console.log(result);
+
+  const batch = [];
+
+  fs.createReadStream(
+    "./datasets/live_tracking_datasets/states_2021-12-06-00.csv",
+  )
+    .pipe(csv())
+    .on("data", (row) => {
+      if (row.lat && row.lon) {
+        batch.push({
+          time: Number(row.time),
+          icao24: row.icao24,
+          lat: Number(row.lat),
+          lon: Number(row.lon),
+          velocity: Number(row.velocity),
+          heading: Number(row.heading),
+          vertrate: Number(row.vertrate),
+          onground: row.onground,
+          baroaltitude: Number(row.baroaltitude),
+          geoaltitude: Number(row.geoaltitude),
+        });
+
+        if (batch.length === 1000) {
+          console.log("inserting with length: 1000 ", batch.length);
+
+          collection.insertMany(batch.splice(0, 1000));
+        }
+      }
+    })
+    .on("end", async () => {
+      console.log("last batch to insert with length: ", batch.length);
+
+      if (batch.length) await collection.insertMany(batch);
+
+      console.log(" insertion Done");
+    });
 }
 
 run();
