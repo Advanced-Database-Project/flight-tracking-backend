5bf0c19dd554cca89c760211c7d4865334c9e0e9
deep <dipvadukiya06@gmail.com>
2026-05-13 17:12:00 +0200

    FTUC01: live flight tracking: stored airport records into the redis

--- services/live-dashboard-service/src/controller/arrivalDepartureByAirport.js
+++ services/live-dashboard-service/src/controller/arrivalDepartureByAirport.js
@@ -1,13 +1,48 @@
 //
 
 import { getArrivalDepartureByAirportData } from "./getAirportData.js";
+import { createClient } from "redis";
 
 // ----------------------------------------
+
+export const redis = createClient();
+
+await redis.connect();
 
 export const getArrivalDepartureByAirport = async (data) => {
   let reponse = {};
 
   reponse = await getArrivalDepartureByAirportData(data);
+
+  for (const flight of reponse[0]?.data) {
+    const key = `arr-${data.city}-${flight.icao24}`;
+
+    await redis.hSet(key, {
+      icao24: flight.icao24 || "",
+      callsign: flight.callsign || "",
+      departure: flight.estDepartureAirport || "",
+      arrival: flight.estArrivalAirport || "",
+      firstSeen: flight.firstSeen || "",
+      lastSeen: flight.lastSeen || "",
+    });
+
+    await redis.expire(key, 3600);
+  }
+
+  for (const flight of reponse[1]?.data) {
+    const key = `dep-${data.city}-${flight.icao24}`;
+
+    await redis.hSet(key, {
+      icao24: flight.icao24 || "",
+      callsign: flight.callsign || "",
+      departure: flight.estDepartureAirport || "",
+      arrival: flight.estArrivalAirport || "",
+      firstSeen: flight.firstSeen || "",
+      lastSeen: flight.lastSeen || "",
+    });
+
+    await redis.expire(key, 3600);
+  }
 
   return reponse;
 };
