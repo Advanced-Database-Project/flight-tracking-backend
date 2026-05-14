00b97147c03f15f9d273e4bc38e2eff3963fd958
DarshanKareliya <kareliyadarshan121@gmail.com>
2026-05-14 15:00:45 +0200

    refactored the code for access token

--- shared/utils.js
+++ shared/utils.js
@@ -288,27 +288,4 @@ export async function findRoute(from, to) {
     }));
 }
 
-// there will be some functions here which are shared by the services
-export const getAccesstoken = async () => {
-  try {
-    const response = await axios.post(
-      "https://auth.opensky-network.org/auth/realms/opensky-network/protocol/openid-connect/token",
-      new URLSearchParams({
-        grant_type: "client_credentials",
-        client_id: env.CLIENT_ID,
-        client_secret: env.CLIENT_SECRET,
-      }),
-    );
 
-    // console.log(response);
-
-    console.log("SUCCESS for getting access token: ", response.data);
-
-    return response.data.access_token;
-  } catch (err) {
-    console.log("ERROR getting  live flight tracking data from API ");
-    console.log("ERRORRRRRRRRRRRRR: ", err.message);
-  }
-};
-
-getAccesstoken();
